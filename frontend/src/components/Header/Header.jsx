20ef3cf972b5fe428f0e7d33fda409f335ae461a
islem-haddada <mohamedfakherelislemhaddada@gmail.com>
2024-07-10 03:54:03 +0100

    start to create navbar and home page

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Order your favourite food here</h2>
+            <p>jklbldsbvbsdvibfibvlkdbvliudv hiunp çvph vphpçvhdsibviobdpv dshvb xicbn  çdnpwpudhfvb oixcv dçd vdhp</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
