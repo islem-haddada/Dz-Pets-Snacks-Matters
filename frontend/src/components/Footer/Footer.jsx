16bd714212d4eca484197e6670f6a3c5e18ee83e
islem-haddada <mohamedfakherelislemhaddada@gmail.com>
2024-07-11 23:12:41 +0100

    sam update

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,20 +1,39 @@
 import React from 'react'
 import './Footer.css'
+import { assets } from '../../assets/assets'
 
 const Footer = () => {
   return (
     <div className='footer' id='footer'>
         <div className="footer-content">
             <div className="footer-content-left">
-
+                <img src={assets.logo} alt="" />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, quos quibusdam? Nulla quos eaque illo tempore consectetur aperiam harum accusamus earum. Eaque accusamus sed ut illo eos repellat reiciendis? Ipsum.</p>
+                <div className="footer-social-icons">
+                    <img src={assets.facebook_icon} alt="" />
+                    <img src={assets.twitter_icon} alt="" />
+                    <img src={assets.linkedin_icon} alt="" />
+                </div>
             </div>
             <div className="footer-content-center">
-
+                <h2>COMPANY</h2>
+                <hl>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Delivery</li>
+                    <li>Privacy policy</li>
+                </hl>
             </div>
             <div className="footer-content-right">
-
+                <h2>GET IN TOUCH</h2>
+                <ul>
+                    <li>+213-000-000-0000</li>
+                    <li>contact@tomato.com</li>
+                </ul>
             </div>
         </div>
+        <hr />
+        <p className="fooder-copyright">Copyright 2024 @ tomato.com - All Right Reserved.</p>
     </div>
   )
 }
