ed82e636882c6e615197ccc48101919d757c96c1
islem-haddada <mohamedfakherelislemhaddada@gmail.com>
2024-07-11 03:08:06 +0100

    style of FoodDisplay

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './FoodDisplay.css'
 import { StoreContext } from '../../context/StoreContext'
+import FoodItem from '../FoodItem/FoodItem'
 
-const FoodDisplay = () => {
+const FoodDisplay = ({category}) => {
 
     const {food_list} = useContext(StoreContext)
 
   return (
-    <div>FoodDisplay</div>
+    <div className='food-display' id='food-display'>
+        <h2>Everything your pet loves right at your fingertips</h2>
+        <div className="food-display-list">
+            {food_list.map((item,index)=>{
+                return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
+            })}
+        </div>
+    </div>
   )
 }
 
