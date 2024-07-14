6b981dc0f4b707f4810c83a378e2974d49a6a527
islem-haddada <mohamedfakherelislemhaddada@gmail.com>
2024-07-14 01:14:54 +0100

    ferst test

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -20,6 +20,8 @@ const StoreContextProvider = (props) => {
         setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
     }
 
+    
+
     useEffect(() => {
         console.log(cartItems);
     }, [cartItems])
