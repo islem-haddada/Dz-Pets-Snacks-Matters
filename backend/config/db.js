118d1c4caa22c3cb1fa2a80f72cc3052e570bfdb
islem-haddada <mohamedfakherelislemhaddada@gmail.com>
2024-07-15 01:13:02 +0100

    darba l9adia

--- backend/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://mohamedfakherelislem:Islem.mongodb1@cluster0.qlwfvgw.mongodb.net/food-del').then(()=>console.log("DB Connected"));
-}
