088ffcf2258aba596fc9f0f7a8d199846d466756
islem-haddada <mohamedfakherelislemhaddada@gmail.com>
2024-07-16 22:27:26 +0100

    add aboutus

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import './Footer.css'
-import { assets } from '../../assets/assets'
+import React from 'react';
+import './Footer.css';
+import { assets } from '../../assets/assets';
 
 const Footer = () => {
   return (
@@ -10,18 +10,24 @@ const Footer = () => {
             <img src={assets.logo} alt="" />
             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
             <div className="footer-social-icons">
-                <img src={assets.facebook_icon} alt="" />
-                <img src={assets.twitter_icon} alt="" />
-                <img src={assets.linkedin_icon} alt="" />
+                <a href="https://www.facebook.com/yourfacebookpage" target="_blank" rel="noopener noreferrer">
+                  <img src={assets.facebook_icon} alt="Facebook" />
+                </a>
+                <a href="https://twitter.com/yourtwitterhandle" target="_blank" rel="noopener noreferrer">
+                  <img src={assets.twitter_icon} alt="Twitter" />
+                </a>
+                <a href="https://www.linkedin.com/company/yourlinkedinpage" target="_blank" rel="noopener noreferrer">
+                  <img src={assets.linkedin_icon} alt="LinkedIn" />
+                </a>
             </div>
         </div>
         <div className="footer-content-center">
             <h2>COMPANY</h2>
             <ul>
-                <li>Home</li>
-                <li>About us</li>
-                <li>Delivery</li>
-                <li>Privacy policy</li>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About us</a></li>
+                <li><a href="/delivery">Delivery</a></li>
+                <li><a href="/privacy">Privacy policy</a></li>
             </ul>
         </div>
         <div className="footer-content-right">
@@ -35,7 +41,7 @@ const Footer = () => {
       <hr />
       <p className="footer-copyright">Copyright 2024 © Tomato.com - All Right Reserved.</p>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
---
+++ frontend/src/pages/AboutUs/AboutUs.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './AboutUs.css'; // Assurez-vous d'avoir créé ce fichier CSS et d'y avoir ajouté les styles nécessaires
+import { assets } from '../../assets/assets';
+
+const AboutUs = () => {
+  return (
+    <div className="about-us">
+      <div className="about-us-content">
+        <div className="about-us-left">
+          <h2>About Us</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+            non mattis risus. Integer pretium fermentum justo, vel feugiat
+            dolor mattis ac.
+          </p>
+          <p>
+            Nulla facilisi. Nulla in mi nec libero maximus euismod. Nam
+            consequat dui eget ligula aliquam interdum.
+          </p>
+          <p>
+            Fusce vitae velit sed ante ultrices efficitur. Quisque eu ligula
+            nec orci vestibulum semper ac ac odio.
+          </p>
+        </div>
+        <div className="about-us-right">
+          <img src={assets.header_img} alt="About Us Image" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
