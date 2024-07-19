4bf0eb68956f4146f1376903547ca2f47a3a1f42
islem-haddada <mohamedfakherelislemhaddada@gmail.com>
2024-07-20 00:51:43 +0100

    add sam text

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -9,7 +9,7 @@ const ExploreMenu = ({category,setCategory}) => {
   return (
     <div className='explore-menu' id='explore-menu'>
       <h1>Explore our menu</h1>
-      <p className='explore-menu-text'>Choose from a diverse menu featuring a delectable array of dishes. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+      <p className='explore-menu-text'>Explore our extensive selection of premium pet foods and supplies. Provide your beloved pets with top-notch nutrition and care.</p>
       <div className="explore-menu-list">
         {menu_list.map((item,index)=>{
             return (
--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -5,8 +5,8 @@ const Header = () => {
   return (
     <div className='header'>
       <div className='header-contents'>
-        <h2>Order your favourite food here</h2>
-        <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+        <h2>Order your pets favorite food here</h2>
+        <p>Discover a wide variety of high-quality pet foods and accessories. Treat your furry friends to the best care they deserve, ensuring their health, happiness, and well-being with our trusted products and exceptional service.</p>
         <a href='#explore-menu'>
           <button>View Menu</button>
         </a>
