fe2e0ec1b523b9d1a800299d35487f1cd1ed08f7
islem-haddada <mohamedfakherelislemhaddada@gmail.com>
2024-07-21 00:47:13 +0100

    modeffer la page home

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import './Header.css';
+import { assets } from '../../assets/assets';
 
 const Header = () => {
   return (
     <div className='header'>
       <div className='header-contents'>
-        <h2>Order your pets favorite food here</h2>
-        <p>Discover a wide variety of high-quality pet foods and accessories. Treat your furry friends to the best care they deserve, ensuring their health, happiness, and well-being with our trusted products and exceptional service.</p>
+        <img src={assets.header_img} alt="Header Image" className='header-img' />
+        <h2>Order your pet's favorite food here</h2>
+        <p>
+          Discover a wide variety of high-quality pet foods and accessories. Treat your furry friends to the best care they deserve, ensuring their health, happiness, and well-being with our trusted products and exceptional service.
+        </p>
         <a href='#explore-menu'>
-          <button>View Menu</button>
+          <button className='view-menu-btn'>View Menu</button>
         </a>
       </div>
     </div>
