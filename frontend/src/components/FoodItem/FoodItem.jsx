ec6c5210d1807b17f618ac7d7c1903ac0f0ff5b4
islem-haddada <mohamedfakherelislemhaddada@gmail.com>
2024-07-25 11:30:35 +0100

    fix assetsjs

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,25 +1,61 @@
-import React, { useContext, useState } from 'react'
-import './FoodItem.css'
-import { assets } from '../../assets/assets'
+import React, { useContext, useRef } from 'react';
+import './FoodItem.css';
+import { assets } from '../../assets/assets';
 import { StoreContext } from '../../Context/StoreContext';
 
-const FoodItem = ({ image, name, price, desc , id }) => {
+const FoodItem = ({ image, name, price, desc, id }) => {
+    const { cartItems, addToCart, removeFromCart, url, currency } = useContext(StoreContext);
+    const imageRef = useRef(null);
 
-    const [itemCount, setItemCount] = useState(0);
-    const {cartItems,addToCart,removeFromCart,url,currency} = useContext(StoreContext);
+    const handleMouseMove = (e) => {
+        const rect = imageRef.current.getBoundingClientRect();
+        const x = (e.clientX - rect.left) / rect.width;
+        const y = (e.clientY - rect.top) / rect.height;
+
+        imageRef.current.style.transformOrigin = `${x * 100}% ${y * 100}%`;
+        imageRef.current.style.transform = 'scale(2.3)'; // Augmentez la valeur du scale
+    };
+
+    const handleMouseLeave = () => {
+        imageRef.current.style.transformOrigin = 'center center';
+        imageRef.current.style.transform = 'scale(1)';
+    };
 
     return (
         <div className='food-item'>
-            <div className='food-item-img-container'>
-                <img className='food-item-image' src={url+"/images/"+image} alt="" />
-                {!cartItems[id]
-                ?<img className='add' onClick={() => addToCart(id)} src={assets.add_icon_white} alt="" />
-                :<div className="food-item-counter">
-                        <img src={assets.remove_icon_red} onClick={()=>removeFromCart(id)} alt="" />
+            <div
+                className='food-item-img-container'
+                onMouseMove={handleMouseMove}
+                onMouseLeave={handleMouseLeave}
+            >
+                <img
+                    ref={imageRef}
+                    className='food-item-image'
+                    src={`${url}/images/${image}`}
+                    alt=""
+                />
+                {!cartItems[id] ? (
+                    <img
+                        className='add'
+                        onClick={() => addToCart(id)}
+                        src={assets.add_icon_white}
+                        alt=""
+                    />
+                ) : (
+                    <div className="food-item-counter">
+                        <img
+                            src={assets.remove_icon_red}
+                            onClick={() => removeFromCart(id)}
+                            alt=""
+                        />
                         <p>{cartItems[id]}</p>
-                        <img src={assets.add_icon_green} onClick={()=>addToCart(id)} alt="" />
+                        <img
+                            src={assets.add_icon_green}
+                            onClick={() => addToCart(id)}
+                            alt=""
+                        />
                     </div>
-                }
+                )}
             </div>
             <div className="food-item-info">
                 <div className="food-item-name-rating">
@@ -29,7 +65,7 @@ const FoodItem = ({ image, name, price, desc , id }) => {
                 <p className="food-item-price">{currency}{price}</p>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default FoodItem
+export default FoodItem;
