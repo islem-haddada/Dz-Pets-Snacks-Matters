1ec57110531a81a5e0aaf61501708e51efde9f4a
islem-haddada <mohamedfakherelislemhaddada@gmail.com>
2024-08-02 19:42:08 +0100

    add sucret key

--- backend/config/db1.js
+++ backend/config/db1.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect('mongodb+srv://islem1:islem123@cluster0.bxst68y.mongodb.net/Zoofood').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://dzPSM:Dz.PSM2024@cluster0.qj6ktzi.mongodb.net/Dz-Pets-Snacks-Matters').then(()=>console.log("DB Connected"));
    
 }
