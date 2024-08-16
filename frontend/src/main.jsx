e13ddc4887647b56c9ea520affcf049e6148024d
islem-haddada <mohamedfakherelislemhaddada@gmail.com>
2024-08-16 18:05:26 +0100

    commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { StoreContextProvider } from './Context/StoreContext';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import StoreContextProvider from './Context/StoreContext'
 
-ReactDOM.render(
-  <StoreContextProvider>
-    <BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <StoreContextProvider>
       <App />
-    </BrowserRouter>
-  </StoreContextProvider>,
-  document.getElementById('root')
-);
+    </StoreContextProvider>
+  </BrowserRouter>,
+)
