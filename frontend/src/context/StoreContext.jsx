366613f5bd0d0d069ef62aa394ebede9d4c5d961
islem-haddada <mohamedfakherelislemhaddada@gmail.com>
2024-08-16 18:25:42 +0100

    update

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "https://dz-pets-snacks-matters-backend.onrender.com"
+    const url = "http://localhost:4000"
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")
