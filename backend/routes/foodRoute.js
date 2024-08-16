02fcd549867a38deffa58494fb1486eae0e6fd83
islem-haddada <mohamedfakherelislemhaddada@gmail.com>
2024-08-16 13:56:03 +0100

    dfb

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -15,7 +15,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage})
 
 foodRouter.get("/list",listFood);
-foodRouter.post("/add",upload.single('image'),addFood);
+foodRouter.post("/add", upload.single('image'), addFood);
 foodRouter.post("/remove",removeFood);
 
 export default foodRouter;
