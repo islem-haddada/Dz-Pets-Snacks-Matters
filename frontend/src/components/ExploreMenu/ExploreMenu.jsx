5c9b78751e84bbb11989edb2d705eb4b9a2b76bb
islem-haddada <mohamedfakherelislemhaddada@gmail.com>
2024-08-17 11:22:19 +0100

    up

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import './ExploreMenu.css'
-import { StoreContext } from '../../context/StoreContext';
+import { StoreContext } from '../../Context/StoreContext';
 
 const ExploreMenu = ({category,setCategory}) => {
 
