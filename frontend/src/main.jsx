8f19fe268d1e4b911e0a06f5bfe5ba2f7acae9be
islem-haddada <mohamedfakherelislemhaddada@gmail.com>
2024-08-17 11:58:46 +0100

    qq

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import StoreContextProvider from './context/StoreContext.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import StoreContextProvider from './context/StoreContext.jsx'; // Assurez-vous que le chemin correspond à la casse correcte
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <StoreContextProvider>
-      <App />
-    </StoreContextProvider>
-  </BrowserRouter>,
-)
+  <React.StrictMode>
+    <BrowserRouter>
+      <StoreContextProvider>
+        <App />
+      </StoreContextProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
