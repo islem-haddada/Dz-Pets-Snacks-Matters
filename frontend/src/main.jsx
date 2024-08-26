04478ebd1726fbfdb69a0c6ac59bd03d7e1a6079
islem-haddada <mohamedfakherelislemhaddada@gmail.com>
2024-08-26 16:15:56 +0100

    fix

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import StoreContextProvider from './context/StoreContext.jsx'; // Assurez-vous que le chemin correspond à la casse correcte
+import StoreContextProvider from './Context/StoreContext.jsx'; // Assurez-vous que le chemin correspond à la casse correcte
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
